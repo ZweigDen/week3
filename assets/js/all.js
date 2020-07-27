2aa05d09a07cc260adb5c5250c9804360a46a266
ZweigDen <65008094+ZweigDen@users.noreply.github.com>
2020-07-27 17:21:21 +0800

    Commit to master

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,55 +7,38 @@ new Vue({
         category: '策略',
         title: '農家樂',
         rank: 23,
-        weight:3.64,
+        weight: 3.64,
         description: '工人放置',
         content: '玩家扮演農夫種菜養動物不要讓自己餓死',
         textNeed: 1,
         imageUrl: '',
       },
       {
-          id: 1196934917910,
-          category: '派對',
-          title: '說書人',
-          rank: 201,
-          weight:1.25,
-          description: '畫畫遊戲',
-          content: '搞笑的派對遊戲，畫技越爛越好笑',
-          textNeed: 0,
-          imageUrl: '',
-        },
-        {
-          id: 1196934917950,
-          category: '競標',
-          title: '瘋王堡',
-          rank: 120,
-          weight:2.66,
-          description: '擺放板塊',
-          content: '充滿心機',
-          textNeed: 0,
-          imageUrl: '',
-        }
+        id: 1196934917910,
+        category: '派對',
+        title: '說書人',
+        rank: 201,
+        weight: 1.25,
+        description: '畫畫遊戲',
+        content: '搞笑的派對遊戲，畫技越爛越好笑',
+        textNeed: 0,
+        imageUrl: '',
+      },
+      {
+        id: 1196934917950,
+        category: '競標',
+        title: '瘋王堡',
+        rank: 120,
+        weight: 2.66,
+        description: '擺放板塊',
+        content: '充滿心機',
+        textNeed: 0,
+        imageUrl: '',
+      }
     ],
     tempProduct: {},
   },
   methods: {
-    updateProduct() {
-      if (this.tempProduct.id) {
-        const id = this.tempProduct.id;
-        this.products.forEach((item, i) => {
-          if (item.id === id) {
-            this.products[i] = this.tempProduct;
-          }
-        });
-      } else {
-        // Unix Timestamp
-        const id = new Date().getTime();
-        this.tempProduct.id = id;
-        this.products.push(this.tempProduct);
-      }
-      this.tempProduct = {};
-      $('#productModal').modal('hide');
-    },
     openModal(isNew, item) {
       switch (isNew) {
         case 'new':
@@ -67,25 +50,37 @@ new Vue({
           $('#productModal').modal('show');
           break;
         case 'delete':
-          $('#delProductModal').modal('show');
           this.tempProduct = Object.assign({}, item);
-          break;
-        default:
+          $('#delProductModal').modal('show');
           break;
       }
     },
-    delProduct() {
+    updateProduct() {
       if (this.tempProduct.id) {
         const id = this.tempProduct.id;
         this.products.forEach((item, i) => {
           if (item.id === id) {
-            this.products.splice(i, 1);
-            this.tempProduct = {};
+            this.products[i] = this.tempProduct;
           }
         });
+      } else {
+        const id = new Date().getTime();
+        this.tempProduct.id = id;
+        this.products.push(this.tempProduct);
       }
-      $('#delProductModal').modal('hide');
+      this.tempProduct = {};
+      $('#productModal').modal('hide');
     },
+    delProduct() {
+      const id = this.tempProduct.id;
+      this.products.forEach((item, i) => {
+        if(item.id === id){
+          this.products.splice(i, 1);
+          this.tempProduct = {};
+        }
+      });
+      $('#delProductModal').modal('hide');
+    }
   },
 });
 
